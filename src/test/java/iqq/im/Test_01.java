c5b730a7a18a7db2b22180a59a9c3391614400f8
leegean <569398403@qq.com>
2014-11-27 10:01:05 +0800

    add class Test_01 Test_02

---
+++ src/test/java/iqq/im/Test_01.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package iqq.im;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Test_01 {
+
+	private int age;
+}
---
+++ src/test/java/iqq/im/Test_02.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package iqq.im;
+
+/**
+ * @author Administrator
+ *2014-11-27
+ */
+public class Test_02 {
+	
+	/**
+	 * 
+	 */
+	private void display() {
+		// FIXME Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	private void getAge() {
+		// FIXME Auto-generated method stub
+
+	}
+	
+	/**
+	 * 
+	 */
+	private void getH() {
+		// XXX Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	private void getA() {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	private void getD() {
+		// XXX Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	private void getT() {
+		// XXX Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	private void getU() {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	private void getI() {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	private void getTO() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private int id;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+}
